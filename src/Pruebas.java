eb3e76f099a3a8f811c70d44678467f19f675c60
unknown <juan.cadavid-p@labinfo.is.escuelaing.edu.co>
2022-02-16 14:54:35 -0500

    Taller 3

---
+++ src/Pruebas.java
@@ -0,0 +1,16 @@
+public class Pruebas {
+
+    public static void m1() {}
+
+    @Test
+    public static void m2() {throw new RuntimeException("Error m2");}
+
+    @Test
+    public static void m3() {}
+
+    @Test
+    public static void m4() {throw new RuntimeException("Error m4");}
+
+    public static void m5() {}
+
+}
---
+++ src/RunTests.java
@@ -0,0 +1,21 @@
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+
+
+public class RunTests {
+    public static void main(String[] args) throws Exception {
+        int passed = 0, failed = 0;
+        for (Method m : Class.forName(args[0]).getMethods()) {
+            if (m.isAnnotationPresent(Test.class)) {
+                try {
+                    m.invoke(null);
+                    passed++;
+                } catch (Throwable ex) {
+                    System.out.printf("Test %s failed: %s %n", m, ex.getCause());
+                    failed++;
+                }}}
+        System.out.printf("Passed: %d, Failed %d%n", passed, failed);
+    }}
